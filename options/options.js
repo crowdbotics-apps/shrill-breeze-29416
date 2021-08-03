ea3b9aa589b553a3059624026fae873ba0402a7f
Crowdbotics <team@crowdbotics.com>
2021-08-03 19:22:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shrill_breeze_29416Identifier",
+  url: "https://shrill_breeze_29416Identifier.botics.co",
+  api: "https://shrill_breeze_29416Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
